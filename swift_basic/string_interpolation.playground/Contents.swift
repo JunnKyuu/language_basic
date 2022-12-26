b5122084de59db7d6dfe0ef72b919aa40f1ffa24
JunnKyuu <junkyu.dev@gmail.com>
2022-12-26 20:21:05 +0900

    swift_string interpolation

---
+++ swift_basic/string_interpolation.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+
+// string interpolation 문자열 보간법
+// 변수 또는 상수 등의 값을 문자열 내에 나타내고 싶을 때 사용
+
+
+var name : String = "Jun"
+print("name : \(name)")
+
+var age : Int = 26
+print("age : \(age)")
