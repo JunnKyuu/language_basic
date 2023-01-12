8a2af07c861cdd220cd0da7fb7c30d9d8ff1b129
JunnKyuu <junkyu.dev@gmail.com>
2023-01-12 23:52:40 +0900

    swift review

---
+++ swift_basic/review.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Cocoa
+
+let a : Int = 200
+
+var b : Int = 300
+b = 200
+
+var someInt : Int = 100
+
+var someUInt : UInt = 200
+
+var someFloat : Float = 1.1
+someFloat = 1
+
+var someDouble : Double = 1.1
+someDouble = 1
+
+var someBool : Bool = false
+someBool = true
+
+var someCharacter : Character = "J"
+someCharacter = "준"
+someCharacter = "🐢"
+
+var someString : String = "JunKyu"
+
+var someAny : Any = -100
+
+var someValue = "Jun"
