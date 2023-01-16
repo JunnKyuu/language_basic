b38f3f03d4f43e5bfad3a4d9cc6f94597c31e957
JunnKyuu <junkyu.dev@gmail.com>
2023-01-16 17:26:40 +0900

    javascript 자료형

---
+++ javascript_basic/type.js
@@ -0,0 +1,31 @@
+// Number type
+let n1 = 1234;
+let n2 = 5.678;
+
+// String type
+let s1 = "hello";
+let s2 = 'world'
+let s3 = `${s1} ${s2}`;
+
+console.log(s3);
+
+// Boolean type
+let b1 = true;
+let b2 = false;
+
+// Null type
+let n = null;
+
+// Undefined type
+let u1;
+let u2 = undefined;
+
+// Array type
+let a1 = [1, 2, 3, 4];
+console.log(a1);
+console.log(a1[0]);
+
+// Object type
+let obj = {a : "apple", b : "banana", c: "cherry"};
+console.log(obj);
+console.log(obj.a);
