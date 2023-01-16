1df26dbd7dc7caf13b0abbf58697929cada3ee73
JunnKyuu <junkyu.dev@gmail.com>
2023-01-16 17:47:50 +0900

    javascript 함수

---
+++ javascript_basic/function.js
@@ -0,0 +1,13 @@
+//function statement
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(10, 20));
+
+// arrow function
+const multiply = (a, b) => {
+  return a * b;
+}
+
+console.log(multiply(10, 20));
